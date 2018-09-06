5a030e826c83fa30895f0cc9d3237f8996d78dbc
Việt Trần <viettd1190@gmail.com>
2018-09-06 17:08:59 +0700

    Init project

---
+++ DashboardReport.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using DashboardReport.WebApp.Models;
+
+namespace DashboardReport.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View(new LogOnModel());
+        }
+
+        [HttpPost]
+        public ActionResult Login(LogOnModel model)
+        {
+            return View(model);
+        }
+    }
+}
